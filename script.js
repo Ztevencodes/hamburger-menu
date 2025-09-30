1e9d5165fbf844046934025fe23e8301a8c83f0a
Steven D. Hernandez <83190360+Ztevencodes@users.noreply.github.com>
2025-09-29 22:47:24 -0400

    reading all file types

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
-
-    toggleButton.addEventListener('click', function() {
-        mobileMenu.classList.toggle('active');
-    });
-});
